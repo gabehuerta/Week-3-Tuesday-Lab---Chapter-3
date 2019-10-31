8ec819d38fe5c5be07e5ad7241d2a6f22d1c5028
gabehuerta <54081519+gabehuerta@users.noreply.github.com>
2019-10-31 17:02:20 -0500

    Add files via upload

---
+++ DivisibleX.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DivisibleX
+{
+    public static void main(String[] args)
+    {
+	Scanner keyboard = new Scanner(System.in);
+
+	System.out.println("Enter two numbers and I will tell you whether your first number is divisible by the second with a remainder of 0");
+	System.out.println("Please enter the first number");
+	int x = keyboard.nextInt( );
+	System.out.println("Please enter the second number");
+	int y = keyboard.nextInt( );
+	
+	if (x % y == 0) {
+		System.out.println("Yes");
+	}
+	else {
+		System.out.println("No");
+	}
+    }
+}
+	
---
+++ ReadInt.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReadInt
+{
+    public static void main(String[] args)
+    {
+	Scanner keyboard = new Scanner(System.in);
+	System.out.println("Please enter a number");
+	System.out.println("I will tell you YES if your number is higher than 100 and or is in between 50 to 75");
+	System.out.println("I will tell you NO if otherwise");
+	int x = keyboard.nextInt( );
+
+	if (x > 100) {
+		System.out.println("YES");
+	}
+	else if (x >= 50 && x <= 75) {
+		System.out.println("YES");
+	} 
+	else {
+		System.out.println("NO");
+	}
+    }
+}
